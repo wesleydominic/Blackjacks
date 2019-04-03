4092fd2bfc15594c4f51bf2039e2748de2eb825e
dominicw-Sheridan <dominicw@sheridancollege.ca>
2019-04-02 22:49:46 -0400

    This is my Deck Class.

---
+++ src/blackjacks/Deck.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjacks;
+
+/**
+ *
+ * @author Wesley
+ */
+public class Deck {
+    //this is Deck Class
+}
