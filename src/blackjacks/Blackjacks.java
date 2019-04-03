655821b967dcdc704420c6b499cb16d9e443540c
dominicw-Sheridan <dominicw@sheridancollege.ca>
2019-04-02 22:48:52 -0400

    This is my main Blackjack Class.

---
+++ src/blackjacks/Blackjacks.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjacks;
+
+/**
+ *
+ * @author Wesley
+ */
+public class Blackjacks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //This is my main
+    }
+    
+}
