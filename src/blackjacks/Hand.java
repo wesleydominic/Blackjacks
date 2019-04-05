ef900745a99dadc22c1f333b90c0de7cd804fdc8
esoimema <ayodejiesoimeme@gmail.com>
2019-04-05 17:11:52 -0500

    updated hand class

--- src/blackjacks/Hand.java
+++ src/blackjacks/Hand.java
@@ -3,8 +3,22 @@ package blackjacks;
 
 /**
  *
- * @author Wesley
+ * @author ayodeji
  */
 public class Hand {
+     protected int points;
+    private int chips;
+    private int aces;
+    public Hand() {
+
+    }
+
+    public Hand(Chip chip) {
+
+    }
+    
+    public int getPoints(){
+        return this.points;
+    }
     //Hand class
 }
