597328b171f23a3141f17733e7a325e869ce16b9
esoimema <ayodejiesoimeme@gmail.com>
2019-04-20 22:07:48 -0500

    Card class implemented

--- src/blackjacks/Card.java
+++ src/blackjacks/Card.java
@@ -10,21 +10,39 @@ package blackjacks;
  * @author dhruvil
  */
 public class Card {
-    
-    public String rank;
-    public String suit;
-    
+     private String rank;
+    private String suit;
     
     public static final String [] SUITS = {"Spades","Clubs","Diamonds","Hearts"};
-     public static final String [] RANKS = {"Ace","Two","Three","Four","Five","Six",
+    public static final String [] RANKS = {"Ace","Two","Three","Four","Five","Six",
                                             "Seven","Eight","Nine","Ten","Jack","Queen","King"};
- 
-public int getValue(){
-     int result = 0;
-     return result;
-}
-public String toString(){
-        return this.rank + " of " + this.suit ;
+    
+    private final String [][] values = {{"Ace","11"},{"Two","2"},{"Three","3"},{"Four","4"},
+                                        {"Five","5"},{"Six","6"},{"Seven","7"},{"Eight","8"},
+                                        {"Nine","9"},{"Ten","10"},{"Jack","10"},{"Queen","10"},
+                                        {"King","10"}};    
+    
+    public Card(int rank, int suit){
+        this.rank = Card.RANKS[rank];
+        this.suit = Card.SUITS[suit];
     }
-    //this is my Card class
+    
+    public int getValue(){
+        int result = 0;
+        
+        for(String[] value : values){
+            if(this.rank.equals(value[0])){
+                result = Integer.parseInt(value[1]);
+            }
+        }
+        
+        return result;
+    }
+    
+    public String toString(){
+        return this.rank + " of " + this.suit;
+    }
 }
+//the Card class
+    
+   
