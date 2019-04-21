9b0e7b34f3a28fe3bba6ad41042c0c42d39cd7f8
esoimema <ayodejiesoimeme@gmail.com>
2019-04-20 22:08:58 -0500

    hand class done

--- src/blackjacks/Hand.java
+++ src/blackjacks/Hand.java
@@ -1,14 +1,35 @@
 
 package blackjacks;
 
+
+import java.util.ArrayList;
+
 /**
  *
  * @author ayodeji
  */
-public class Hand {
-     protected int points;
+public class Hand implements Comparable<Hand> {
+
+    //Hand a =new Hand();
+    protected int points;
     private int chips;
     private int aces;
+    private ArrayList<Card> totalCards = new ArrayList<Card>();
+
+    public void receiveCard(Card card) {
+        totalCards.add(card);
+        this.points += card.getValue();
+        
+        if(card.toString().contains("Ace")){
+            this.aces += 1;
+        }
+        
+        while(this.points > 21 && this.aces > 0){
+            this.points -= 10;
+            this.aces -= 1;
+        }
+    }
+
     public Hand() {
 
     }
@@ -20,5 +41,24 @@ public class Hand {
     public int getPoints(){
         return this.points;
     }
-    //Hand class
+
+    public String toString() {
+        return this.totalCards.toString();
+    }
+
+    public int compareTo(Hand hand) {
+        int result = 0;
+
+        if (this.points == hand.points) {
+            result = 0;
+        } else if (this.points >= hand.points) {
+            result = 1;
+        } else if (this.points <= hand.points) {
+            result = -1;
+        }
+
+        return result;
+    }
 }
+//hand class implemented
+     
